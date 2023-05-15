5948435308442ec10fdab80f2a8071c18de9bab2
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-05-15 17:46:16 +0530

    image paths

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function SignUp() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+  return <h1 className='sign-up'>In Production👩‍🚀🏗️🚀</h1>;
 }
